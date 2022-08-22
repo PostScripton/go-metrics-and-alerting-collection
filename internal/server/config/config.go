3c2b5aa9a947a6d3a14f301d14d2924c5f8de6aa
Sergey Podzemelnykh <postscripton.sp@gmail.com>
2022-08-22 11:25:22 +0300

    Increment 7

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -1,20 +1,33 @@
 package config
 
 import (
+	"flag"
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	"time"
 )
 
 type config struct {
-	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
-	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool          `env:"RESTORE" envDefault:"true"`
+	Address       string        `env:"ADDRESS"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL"`
+	StoreFile     string        `env:"STORE_FILE"`
+	Restore       bool          `env:"RESTORE"`
 }
+
+const defaultAddress = "localhost:8080"
+const defaultRestore = true
+const defaultStoreFile = "/tmp/devops-metrics-db.json"
+const defaultStoreInterval = 5 * time.Minute
 
 func NewConfig() *config {
 	var cfg config
+
+	flag.StringVar(&cfg.Address, "a", defaultAddress, "An address of the server")
+	flag.BoolVar(&cfg.Restore, "r", defaultRestore, "Whether restore state from a file")
+	flag.StringVar(&cfg.StoreFile, "f", defaultStoreFile, "A file to store to or restore from")
+	flag.DurationVar(&cfg.StoreInterval, "i", defaultStoreInterval, "An interval for storing into a file")
+
+	flag.Parse()
 	if err := env.Parse(&cfg); err != nil {
 		fmt.Printf("Parsing .env error: %s\n", err)
 		return nil
